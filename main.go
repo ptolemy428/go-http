430ddfc2792bc50c3e15aeee150e6927f05fcdf1
Larry Liang <ptolemy428@gmail.com>
2019-02-23 10:50:10 +1100

    add call out func

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
@@ -10,7 +11,19 @@ func handle(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Home of Go</h1>")
 }
 
+func callOut(w http.ResponseWriter, r *http.Request) {
+	url := "http://www.google.com"
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+	io.Copy(w, res.Body)
+}
+
 func main() {
 	http.HandleFunc("/", handle)
+	http.HandleFunc("/out", callOut)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
